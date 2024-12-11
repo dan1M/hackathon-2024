3b31b97698411da478bf0f002b0a163a8a77f77d
xaless <hsuwang75@gmail.com>
2024-12-11 10:27:13 +0100

    add config supabaseClient

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
 import { useEffect, useState } from 'react';
-
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
+import { supabase } from '../supabaseClient';
 
 function App() {
   const [users, setUsers] = useState([]);
