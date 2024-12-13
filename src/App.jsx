c43c63a34ff4632202104ed6088fac4c9f52f2f9
dan1M <maneadaniel555@gmail.com>
2024-12-13 14:52:28 +0100

    dashboard layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,30 @@
-import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { supabase } from '../supabaseClient';
 import Login from './pages/login';
-import Header from './components/header';
-import Footer from './components/footer';
-import Planning from './pages/Planning'; 
+import Planning from './pages/Planning';
 import Dashboard from './pages/dashboard';
-import Navbar from './components/navbar';
 import HomePage from './pages/HomePage';
 import FilierePage from './pages/filiere';
 import ListeTeachers from './pages/listeTeachers';
-import { supabase } from "../supabaseClient";
-import "./styles/Auth.css";
-
-const App = () => {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    getUsers();
-  }, []);
-
-  const getUsers = async () => {
-    const { data, error } = await supabase.from('users_hackathon').select();
-    if (error) 
-      console.error('error', error);
-    else setUsers(data);
-  };
+import './assets/styles/Auth.css';
+import DashboardLayout from './layout/DashboardLayout';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/homePage" element={<HomePage />} />
-        <Route path="/fields" element={<FilierePage />} />
-        <Route path="/teachers_list" element={<ListeTeachers />} />
-        <Route path="/planning" element={<Planning />} />
-        {/* Ajoute d'autres routes si nécessaire */}
+
+        <Route path="/" element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/homePage" element={<HomePage />} />
+          <Route path="/fields" element={<FilierePage />} />
+          <Route path="/teachers_list" element={<ListeTeachers />} />
+          <Route path="/planning" element={<Planning />} />
+          {/* Ajoute d'autres routes si nécessaire */}
+        </Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
-}
-export default App;
+};
 
+export default App;
