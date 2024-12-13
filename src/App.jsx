7d404db60a92e93e669784cd0f18053fc1140e12
dan1M <maneadaniel555@gmail.com>
2024-12-13 11:52:55 +0100

    installed chakra ui v2 for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
+import { Box, Button } from '@chakra-ui/react';
 import { generatePlanning } from './utils/generate-planning';
 
 function App() {
   return (
-    <>
-      <h1>HACKATHON TEST</h1>
-      <button onClick={() => generatePlanning({ class_id: 1, week: 3 })}>
+    <Box>
+      <Button onClick={() => generatePlanning({ class_id: 1, week: 3 })}>
         Test GENERATE PLANNING
-      </button>
-    </>
+      </Button>
+    </Box>
   );
 }
 
